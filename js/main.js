ea050f4054b4ba93e2a2e3ab59f36544f5940843
kristic <root@DESKTOP-NF3VCI5.localdomain>
2019-11-08 13:51:12 +0100

    fixed up, added geolocation

--- js/main.js
+++ js/main.js
@@ -42,50 +42,10 @@ var list = [
         "latitude": "39.482382"
       }];
 
-class Point{
-    constructor(lat, lng){
-        this.lat = lat;
-        this.lng = lng;
-        // this.name = name;
-        // this.country = country;
-        // this.city = city;
-        // this.linkedIn = linkedIn;
-        
-        // let marker = new google.maps.Marker({
-        //     position:{lat: this.lat ,lng: this.lng},
-        //     map: map,
-        //     icon: '',
-        // });
-
-        //gmarkers.push(marker);
-
-        
-
-
-
-
-        // marker.addListener('click', ()=>{
-        //     // for( let i = 0; i < gmarkers.length; i++){
-        //     //     gmarkers[i].setMap(null);
-        //     // }
-
-        //     infoWindow.open(map, marker);
-        //     console.log("marker created");
-        // });
-
-    
-    }
-
-    // getValues(){
-    //     console.log(this.lat);
-    //     console.log(this.lng);
-    // }
-
-}
 //Creates a google maps api script
 function mapScript(){
     let gmap = document.createElement('script');
-    const API_KEY = "AIzaSyCbi55z8XJ33DfKeJvxI3ccEru05EhofEw";
+    const API_KEY = "AIzaSyAIXx8S4YwRiV7Q4bLbzbR8J4-exix-gNY";
 
     gmap.type = 'text/javascript';
     gmap.src = `https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&key=${API_KEY}&language=en`;
@@ -100,7 +60,6 @@ function initMap(){
     geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), defMapCenter);
 
-
     let infoWindow = new google.maps.InfoWindow({
         content: `  <h3>${this.lat}</h3><br>
                     <h3>${this.lat}</h3><br>
@@ -108,8 +67,24 @@ function initMap(){
                     <h3>${this.lat}</h3>`            
     });
 
+    codeAddress("Belgrade, Serbia");
 
-    //CREATE MARKERS
+    function codeAddress(address) {
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        } else {
+        alert("Geocode unsuccessful");
+        }
+    });
+}
+
+    
+    //CREATE MARKERS from list
     for (let i = 0; i < list.length; i++) {
 
         var marker = new google.maps.Marker({
@@ -126,22 +101,18 @@ function initMap(){
                     '<hr class="grey" />' +
                     list[i].description) + '</div>';
                     infoWindow.open(map, marker);
-                    }
+                }
             }
         })(marker, i));
     }
-
 }
 
-let sheetsUrl = 'https://docs.google.com/spreadsheets/d/1tLuovMCa6C0jQLlTDP3ju3AOtbUSLxD8TBU2n_G5ye4/edit#gid=1405687631/1/public/basic?alt=json'
+let sheetsUrl = 'https://spreadsheets.google.com/feeds/cells/1tLuovMCa6C0jQLlTDP3ju3AOtbUSLxD8TBU2n_G5ye4/1/public/full?alt=json'
 fetch(`${sheetsUrl}`).then( response => {
     return response.json();
 }).then( data => { 
     console.log(data);
 });
-
-
-
 
 
 // fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${API_KEY}`).then( response => {
@@ -153,18 +124,3 @@ fetch(`${sheetsUrl}`).then( response => {
 
 //geo
 // https://gist.github.com/brittanystoroz/5573406
-
-
-// function codeAddress(address) {
-//     geocoder.geocode({ 'address': address }, function(results, status) {
-//         if (status === google.maps.GeocoderStatus.OK) {
-//         map.setCenter(results[0].geometry.location);
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             position: results[0].geometry.location
-//         });
-//         } else {
-//         alert("Geocode unsuccessful");
-//         }
-//     });
-// }
