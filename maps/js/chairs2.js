15be1becb31abf9ea9995056addb518a6c5169ae
Walter Strametz <57393847+wstrametz@users.noreply.github.com>
2020-08-17 17:00:58 +0200

    Update chairs2.js

--- maps/js/chairs2.js
+++ maps/js/chairs2.js
@@ -1,5 +1,5 @@
 const sheetsCsvUrl =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOb3RR1ZisBeRYH_0g3NR7zq1nUUvmTVKuerT3fKwif-SqbR2OfSY7IXNZaNL0SvbOoKrPFrEuZL-i/pub?gid=2025752931&single=true&callback=googleDocCallback&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOb3RR1ZisBeRYH_0g3NR7zq1nUUvmTVKuerT3fKwif-SqbR2OfSY7IXNZaNL0SvbOoKrPFrEuZL-i/pub?gid=2025752931&single=true&callback=googleDocCallback&output=csv&&sheet=publicData";
 
   const API_KEY = "AIzaSyBTCXWdYTqfIF7OJ8GfyT85saKrV7u0Gm0";
 const SIZE = 35;
